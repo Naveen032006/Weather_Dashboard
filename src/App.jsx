6bab86614271ab25bcde07a7237f40af30e7c082
naveen <naveenkumar03202006@gmail.com>
2025-09-23 11:44:19 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Layout } from './layout'
+
+function App() {
+  
+
+  return (
+    <>
+      <Layout/>
+    </>
+  )
+}
+
+export default App
