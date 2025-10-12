688206dcb237c9f0236b5d7525a65355919724b6
GrandWizard1102 <v.kavimani1106@gmail.com>
2025-10-12 22:14:47 +0530

    3rd component

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,6 +2,53 @@ import React from "react";
 import WeatherCard from "./WeatherCard";
 
 function Forecast({ mode, width, id }) {
+  const datas = [
+    {
+      time: "12:00",
+      image1: "./src/assets/cloudy.png",
+      image2: "./src/assets/direction.png",
+      temp: "27°C",
+      speed: "3Km/h",
+      color1: "#F88508",
+      color2: "#F6FAD9",
+    },
+    {
+      time: "12:00",
+      image1: "./src/assets/cloudy (1).png",
+      image2: "./src/assets/direction.png",
+      temp: "27°C",
+      speed: "3Km/h",
+      color1: "#443D64",
+      color2: "#6582C6",
+    },
+    {
+      time: "12:00",
+      image1: "./src/assets/cloudy (2).png",
+      image2: "./src/assets/direction.png",
+      temp: "27°C",
+      speed: "3Km/h",
+      color1: "#F88508",
+      color2: "#F6FAD9",
+    },
+    {
+      time: "12:00",
+      image1: "./src/assets/sun.png",
+      image2: "./src/assets/direction.png",
+      temp: "27°C",
+      speed: "3Km/h",
+      color1: "#443D64",
+      color2: "#6582C6",
+    },
+    {
+      time: "12:00",
+      image1: "./src/assets/weather.png",
+      image2: "./src/assets/direction.png",
+      temp: "27°C",
+      speed: "3Km/h",
+      color1: "#F88508",
+      color2: "#F6FAD9",
+    },
+  ];
   return (
     <>
       <div
@@ -29,51 +76,9 @@ function Forecast({ mode, width, id }) {
           <>
             <h1 style={{ textAlign: "center" }}>Hourly Forecast</h1>
             <div style={{ display: "flex" }}>
-              <WeatherCard
-                time="12:00"
-                image1={"./src/assets/cloudy.png"}
-                image2={"./src/assets/direction.png"}
-                temp="27°C"
-                speed="3Km/h"
-                color1="#F88508"
-                color2="#F6FAD9"
-              />
-              <WeatherCard
-                time="12:00"
-                image1={"./src/assets/cloudy (1).png"}
-                image2={"./src/assets/direction.png"}
-                temp="27°C"
-                speed="3Km/h"
-                color1="#443D64"
-                color2="#6582C6"
-              />
-              <WeatherCard
-                time="12:00"
-                image1={"./src/assets/cloudy (2).png"}
-                image2={"./src/assets/direction.png"}
-                temp="27°C"
-                speed="3Km/h"
-                color1="#F88508"
-                color2="#F6FAD9"
-              />
-              <WeatherCard
-                time="12:00"
-                image1={"./src/assets/sun.png"}
-                image2={"./src/assets/direction.png"}
-                temp="27°C"
-                speed="3Km/h"
-                color1="#443D64"
-                color2="#6582C6"
-              />
-              <WeatherCard
-                time="12:00"
-                image1={"./src/assets/weather.png"}
-                image2={"./src/assets/direction.png"}
-                temp="27°C"
-                speed="3Km/h"
-                color1="#F88508"
-                color2="#F6FAD9"
-              />
+              {datas.map((data, index) => (
+                <WeatherCard key={index} {...data} />
+              ))}
             </div>
           </>
         )}
