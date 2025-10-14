02fd35b3fc3c745e3f33ba57470f38037f65912d
GrandWizard1102 <v.kavimani1106@gmail.com>
2025-10-14 21:00:09 +0530

    My changes

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,97 +1,65 @@
-import { Box, Paper, Stack, Typography } from "@mui/material";
-import WbTwilightOutlinedIcon from "@mui/icons-material/WbTwilightOutlined";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+    import { Box, Paper, Stack, Typography } from "@mui/material";
+    import WbSunnyIcon from '@mui/icons-material/WbSunny';
+    import WbTwilightOutlinedIcon from '@mui/icons-material/WbTwilightOutlined';
+    import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+    import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+    import { Iconbox } from "./iconbox";
+    import { Iconwrap } from "./iconwrap";
 import { Iconbox1 } from "./weather1";
 
-export function Weather({ detail }) {
-  return (
-    <Paper
-      elevation={7}
-      sx={{
-        textAlign: "center",
-        p: { xs: 3, sm: 4, md: 4 },
-        width: { xs: "100%", sm: "70%", md: "50%" },
-        backgroundColor: "#D9D9D9",
-        borderRadius: "30px",
-      }}
-    >
-      <Box sx={{ display: "flex", justifyContent: "space-between", gap: 4 }}>
-        <Stack spacing={1}>
-          <Stack>
-            <Typography
-              variant="h3"
-              sx={{
-                fontWeight: "800",
-                background: "linear-gradient(to bottom, #333, #aaa)",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-              }}
-            >
-              {detail.current.temp_c}°C
-            </Typography>
-            <Stack spacing={0.5} direction="row">
-              <Typography
-                variant="subtitle1"
-                sx={{ fontWeight: "600", color: "#464545ff" }}
-              >
-                Feels like:
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>
-                {detail.current.feelslike_c}°C
-              </Typography>
-            </Stack>
-          </Stack>
+    export function Weather() {
+        return (
+            <Paper  elevation={7} sx={{
+    textAlign: "center",
+    p: { xs: 3, sm: 4, md: 4 },
+    width:{xs:"100%",sm:"70%",md:"50%"},
+    backgroundColor: "#D9D9D9",
+    borderRadius: "30px",
+  }}>
+                <Box sx={{ display:"flex",justifyContent:"space-between",gap:4}}>
+                    <Stack spacing={1}>
+                        <Stack>
+                            <Typography variant="h3" sx={{ fontWeight: "800", background: "linear-gradient(to bottom, #333, #aaa)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent", }}>24°C</Typography>
+                            <Stack spacing={0.5} direction="row">
+                                <Typography variant="subtitle1" sx={{ fontWeight: "600", color: "#464545ff" }}>Feels like:</Typography>
+                                <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>22°C</Typography>
+                            </Stack>
+                        </Stack>
 
-          <Stack spacing={1} direction="row">
-            <Stack spacing={0}>
-              <ArrowUpwardIcon />
-              <WbTwilightOutlinedIcon />
-            </Stack>
-            <Stack>
-              <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>
-                Sunrise
-              </Typography>
-              <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
-                {detail.forecast.forecastday[0].astro.sunrise}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Stack spacing={1} direction="row">
-            <Stack spacing={0}>
-              <ArrowDownwardIcon />
-              <WbTwilightOutlinedIcon />
-            </Stack>
-            <Stack>
-              <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>
-                Sunset
-              </Typography>
-              <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
-                {detail.forecast.forecastday[0].astro.sunset}
-              </Typography>
-            </Stack>
-          </Stack>
-        </Stack>
-        <Stack sx={{ padding: "1rem" }}>
-          <Stack spacing={1}>
-            <img src={detail.current.condition.icon} width={150} height={150} />
-            <Typography
-              variant="h5"
-              sx={{ fontWeight: "600", textAlign: "center", padding: "1rem" }}
-            >
-              {detail.current.condition.text}
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack>
-          <Iconbox1
-            humidity={`${detail.current.humidity}%`}
-            windspeed={`${detail.current.wind_kph}Km/h`}
-            pressure={`${detail.current.pressure_mb}hpa`}
-            uv={`${detail.current.uv}`}
-          />
-        </Stack>
-      </Box>
-    </Paper>
-  );
-}
+                        <Stack spacing={1} direction="row">
+                            <Stack spacing={0}>
+                                <ArrowUpwardIcon />
+                                <WbTwilightOutlinedIcon />
+                            </Stack>
+                            <Stack>
+                                <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>Sunrise</Typography>
+                                <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>06:20 AM</Typography>
+                            </Stack>
+                        </Stack>
+                        <Stack spacing={1} direction="row">
+                            <Stack spacing={0}>
+                                <ArrowDownwardIcon />
+                                <WbTwilightOutlinedIcon />
+                            </Stack>
+                            <Stack>
+                                <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>Sunrise</Typography>
+                                <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>06:20 AM</Typography>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                    <Stack sx={{padding:"1rem"}} >
+                        <Stack spacing={3}>
+                            <WbSunnyIcon sx={{fontSize:"9rem" ,color:"#EDC43E"}}/>
+                            <Typography variant="h5" sx={{fontWeight: "600",textAlign:"center",padding:"1rem"}}>Sunny</Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack >
+                        <Iconbox1 humidity="40%" windspeed="2km/hr" pressure="990hpa" uv="10"/>
+                    </Stack>
+                    
+                
+                </Box>
+            </Paper>
+
+        );
+    }
--- src/layout.jsx
+++ src/layout.jsx
@@ -2,9 +2,25 @@ import { Box } from "@mui/material";
 import { Time } from "./components/time";
 import { Weather } from "./components/weather";
 import Forecast from "./components/Forecast";
+import { useState, useEffect } from "react";
 
 export function Layout() {
-  return (
+  const apikey = "016692c1b92044a0b25163019251310";
+  const city = "Chennai";
+  const [value, setvalue] = useState(null);
+  const fetchdata = () => {
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${apikey}&q=${city}&aqi=no`
+    )
+      .then((response) => response.json())
+      .then((data) => setvalue(data))
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  console.log(value?.current);
+  return value ? (
     <>
       <Box sx={{ display: "flex", justifyContent: "center", gap: 4 }}>
         <Time />
@@ -20,8 +36,10 @@ export function Layout() {
         }}
       >
         <Forecast mode="light" width="20%" id="days" detail={value} />
-        <Forecast mode="light" width="60%" id="hour" />
+        <Forecast mode="light" width="60%" id="hour" detail={value} />
       </Box>
     </>
+  ) : (
+    <h1>Data retrieving</h1>
   );
 }
